f524a824e578e38bb0848b0cdce7a7c93977170f
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2025-06-23 19:01:35 +0000

    doc: RedBlackNode

--- include/Trees/RedBlack/RedBlackNode.hpp
+++ include/Trees/RedBlack/RedBlackNode.hpp
@@ -11,10 +11,21 @@ struct RedBlackNode : public Node<Key, Value> {
     RedBlackNode* parent;
     Color color;
 
+    /**
+     * @brief Constructs a RedBlackNode.
+     *
+     * @param k The key for the node.
+     * @param v The value for the node.
+     * @param l Pointer to the left child node.
+     * @param r Pointer to the right child node.
+     * @param p Pointer to the parent node.
+     * @param c The color of the node (Red or Black).
+     */
     RedBlackNode(const Key& k, const Value& v, 
-                RedBlackNode* l, RedBlackNode* r, 
-                RedBlackNode* p, Color c)
-    : Node<Key, Value>(k, v), left(l), right(r), parent(p), color(c) {}
+                    RedBlackNode* l, RedBlackNode* r, 
+                    RedBlackNode* p, Color c)
+        : Node<Key, Value>(k, v), left(l), right(r), parent(p), color(c) {}
+
 };
 
 #endif
