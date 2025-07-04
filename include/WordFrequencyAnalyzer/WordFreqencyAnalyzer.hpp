da512d9a03017d032c381ca142c2174f5c5932b6
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-07-03 21:32:27 -0300

    wip

---
+++ include/WordFrequencyAnalyzer/WordFreqencyAnalyzer.hpp
@@ -0,0 +1,11 @@
+#ifndef WORD_FREQUENCY_ANALYZER_HPP
+#define WORD_FREQUENCY_ANALYZER_HPP
+
+#include "Reports/ReportData.hpp"
+
+class WordFrequencyAnalyzer {
+public:
+    ReportData 
+};
+
+#endif
