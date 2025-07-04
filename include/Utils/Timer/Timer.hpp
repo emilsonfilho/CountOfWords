65d65ed9b773efa74c948f06c53577b305daf2d3
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-07-03 21:49:22 -0300

    feat: timer

---
+++ include/Utils/Timer/Timer.hpp
@@ -0,0 +1,16 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <chrono>
+
+class Timer {
+    std::chrono::high_resolution_clock::time_point begin, end;
+
+    std::chrono::high_resolution_clock::time_point now() const;
+public:
+    void start();
+    void stop();
+    long long duration() const;
+};
+
+#endif
