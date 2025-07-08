860d5be18d0f0bc91afc545a30d46157f155e62e
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2025-07-08 16:37:00 +0000

    docs: locale

--- include/Configs/Locale/Locale.hpp
+++ include/Configs/Locale/Locale.hpp
@@ -5,9 +5,25 @@
 
 inline const char * defaultLocale = "pt_BR.utf8"; // Default locale for Brazilian Portuguese
 
+/**
+ * @class Locale
+ * @brief Provides utilities related to language and locale settings.
+ *
+ * This class offers static methods to retrieve locale-specific information,
+ * such as the current language setting of the system or application.
+ */
 class Locale {
 public:
-  static const char *getLang();
+    /**
+     * @brief Returns the current language setting.
+     *
+     * This static method retrieves the language code (e.g., "en", "pt") based on
+     * the current system or application locale configuration.
+     *
+     * @return A pointer to a C-style string representing the language code.
+     */
+    static const char *getLang();
 };
+
 
 #endif
